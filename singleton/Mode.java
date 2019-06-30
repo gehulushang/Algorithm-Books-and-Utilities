9f986c1765192e917091fe9aca0fe2d9668e6bf6
朱剑锋 <2445843238@qq.com>
2019-06-30 21:31:47 +0800

    Update Mode.java

--- singleton/Mode.java
+++ singleton/Mode.java
@@ -4,7 +4,7 @@ package singleton;
 //在获取类实例的方法上加同步锁，并且给类实例对象加上volatile修饰符
 public class Mode {
 
-    private static volatile Mode Mode;
+    private static volatile Mode sMode;
 
     private Mode() {
         System.out.println("create " + getClass().getSimpleName());
@@ -12,11 +12,11 @@ public class Mode {
 
     public static Mode getInstance(){
         synchronized (Mode.class) {
-            if (Mode == null) {
-                Mode = new Mode();
+            if (sMode == null) {
+                sMode = new Mode();
             }
         }
-        return Mode;
+        return sMode;
     }
 
     public static void main(String[] args) {
