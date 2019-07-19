1598999a5dc3fcedb0ac7eba898c4c8ba25f6bee
朱剑锋 <2445843238@qq.com>
2019-07-19 10:02:42 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,108 +1,58 @@
+#include <stdio.h>
+#include <math.h>
+#include <sstream>
 #include <iostream>
-#include <algorithm>
 #include <vector>
-
-
-
-//DFS+Dijkstra
-
-
-
+#include <map>
+#include <set>
 using namespace std;
-int n, m, s, d;
-int e[510][510], dis[510], cost[510][510];
-vector<int> pre[510];
-bool visit[510];
-const int inf = 99999999;
-vector<int> path, temppath;
-int mincost = inf;
-void dfs(int v){
-	temppath.push_back(v);
-	if(v==s){
-		int tempcost = 0;
-		for(int i = temppath.size() - 1;i>0;i--){
-			int id = temppath[i],nextid = temppath[i-1];
-			tempcost += cost[id][nextid];
-			
-		}
-		if(tempcost < mincost){
-			mincost = tempcost;
-			path = temppath;
-			
-		}
-		temppath.pop_back();
-		return ;
-		 
-	} 
-	for(int i = 0;i<pre[v].size() ;i++){
-		dfs(pre[v][i]);
+
+vector<int> v;
+
+int a[1010], n, isMax = 1, isMin = 1;
+void dfs(int index){
+	if(index*2+1>n&&index*2>n){
 		
-	} 
-	temppath.pop_back();
-	
+		if(index<=n){
+			for(int i = 0;i<v.size() ;i++){
+				printf("%d%s", v[i], i != v.size() - 1 ? " " : "\n");
+			}
+		}
+		
+	}else{
+		
+		v.push_back(a[index*2+1]);
+		dfs(index*2+1);
+		v.pop_back();
+		
+		v.push_back(a[index*2]);
+		dfs(index*2);
+		v.pop_back();
+		  
+	}
 }
 int main(){
-	fill(e[0], e[0] + 510 * 510, inf);
-	fill(dis, dis + 510, inf);
-	scanf("%d%d%d%d", &n, &m, &s, &d);
-	for(int i = 0;i<m;i++){
-		int a,b;
-		cin>>a>>b;
-		cin>>e[a][b];
-		e[b][a] = e[a][b];
-		cin>>cost[a][b];
-		cost[b][a] = cost[a][b];
-		
+	cin>>n;
+	for(int i = 1;i<=n;i++){
+		cin>>a[i];
 	}
-	pre[s].push_back(s);
-	dis[s] = 0;
-	for(int i = 0;i<n;i++){
-		int u = -1,minn = inf;
-		for(int j = 0;j<n;j++){
-			if(visit[j]==false&&dis[j]<minn){
-				u = j;
-				minn = dis[j];
-				
-			}
-		}
-		if(u==-1){
-			break;
+	v.push_back(a[1]);
+	dfs(1);
+	for(int i = 2;i<=n;i++){
+		if(a[i/2]>a[i]){
+			isMin = 0;
 			
 		}
-		visit[u] = true;
-		for(int v = 0;v<n;v++){
-			if(visit[v]==false&&e[v][u]!=inf){
-				if(dis[v]>dis[u]+e[u][v]){
-					dis[v] = dis[u] + e[u][v];
-					pre[v].clear() ;
-					pre[v].push_back(u);
-					 
-				}else if(dis[v]==dis[u]+e[u][v]){
-					pre[v].push_back(u);
-				}
-			}
+		if(a[i/2]<a[i]){
+			isMax = 0;
 		}
-	} 
-	dfs(d);
-	for(int i = path.size() - 1; i >= 0; i--)
-		printf("%d ", path[i]);
-	printf("%d %d", dis[d], mincost);
+	}
+	if (isMin == 1)
+		printf("Min Heap");
+	else
+		printf("%s", isMax == 1 ? "Max Heap" : "Not Heap");
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
