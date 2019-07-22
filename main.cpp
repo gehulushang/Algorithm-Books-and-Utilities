b74bf8d30d7e7a71f1b12be98ae3a0aba7a014b2
朱剑锋 <2445843238@qq.com>
2019-07-22 13:42:30 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,58 +1,38 @@
-#include <stdio.h>
-#include <math.h>
-#include <sstream>
 #include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-using namespace std;
-//回溯算法实现
-vector<int> v;
+#include <algorithm>
+#include <cstring>
 
-int a[1010], n, isMax = 1, isMin = 1;
-void dfs(int index){
-	if(index*2+1>n&&index*2>n){
-		
-		if(index<=n){
-			for(int i = 0;i<v.size() ;i++){
-				printf("%d%s", v[i], i != v.size() - 1 ? " " : "\n");
-			}
-		}
-		
-	}else{
-		
-		v.push_back(a[index*2+1]);
-		dfs(index*2+1);
-		v.pop_back();
-		
-		v.push_back(a[index*2]);
-		dfs(index*2);
-		v.pop_back();
-		  
-	}
-}
+using namespace std; 
+
+
+long long convert(string n, long long radix) {    
+	long long sum = 0;    
+	int index = 0, temp = 0;    
+	for (auto it = n.rbegin(); it != n.rend(); it++) {        
+	temp = isdigit(*it) ? *it - '0' : *it - 'a' + 10;       
+		sum += temp * pow(radix, index++);   
+    }    
+	return sum; 
+} 
+
 int main(){
-	cin>>n;
-	for(int i = 1;i<=n;i++){
-		cin>>a[i];
-	}
-	v.push_back(a[1]);
-	dfs(1);
-	for(int i = 2;i<=n;i++){
-		if(a[i/2]>a[i]){
-			isMin = 0;
-			
-		}
-		if(a[i/2]<a[i]){
-			isMax = 0;
-		}
-	}
-	if (isMin == 1)
-		printf("Min Heap");
-	else
-		printf("%s", isMax == 1 ? "Max Heap" : "Not Heap");
+	
+	string num;
+	int radix;
+	cin>>num>>radix;
+	
+	int ans = convert(num,radix);
+	cout<<ans;
+	
 	return 0;
 }
+
+
+
+
+
+
+
 
 
 
