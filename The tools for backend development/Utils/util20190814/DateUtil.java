761bd60441f22b863ebe633e000654c0308c118e
朱剑锋 <2445843238@qq.com>
2019-08-14 18:21:09 +0800

    Add files via upload

---
+++ The tools for backend development/Utils/util20190814/DateUtil.java
@@ -0,0 +1,35 @@
+import com.sun.deploy.util.StringUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+
+    public static String formatDate(Date date,String format){
+        String result = "";
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        if(date!=null){
+            result = sdf.format(date);
+
+        }
+        return result;
+
+    }
+
+    public static Date formatString(String str,String format) throws ParseException {
+        if(StringUtil.isEmpty(str)){
+            return null;
+
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.parse(str);
+
+    }
+
+    public static String getCurrentDateStr(){
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(date);
+    }
+}
