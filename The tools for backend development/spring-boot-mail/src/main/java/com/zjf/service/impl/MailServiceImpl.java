27a662f732f4c63948dd8e7aeab86ba4887d38b8
朱剑锋 <2445843238@qq.com>
2019-08-15 10:31:49 +0800

    Create  MailServiceImpl.java

---
+++ The tools for backend development/spring-boot-mail/src/main/java/com/zjf/service/impl/MailServiceImpl.java
@@ -0,0 +1,140 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+
+@Component
+public class MailServiceImpl implements MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${mail.fromMail.addr}")
+    private String from;
+
+    /**
+     * 发送文本邮件
+     * @param to
+     * @param subject
+     * @param content
+     */
+    @Override
+    public void sendSimpleMail(String to, String subject, String content) {
+
+        /**
+         * SimpleMailMessage是值对象，封装了一些简单的属性，如from、to、subject、text等等，
+         * SimpleMailMessage只能用来发送text格式的邮件
+         */
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+
+        try{
+            mailSender.send(message);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 发送html邮件
+     * @param to
+     * @param subject
+     * @param content
+     */
+    @Override
+    public void sendHtmlMail(String to, String subject, String content) {
+
+        /**
+         *
+         */
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            //true表示需要创建一个multipart message
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+
+            mailSender.send(message);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 发送带有附件的文件
+     * @param to
+     * @param subject
+     * @param content
+     * @param filePath
+     */
+    @Override
+    public void sendAttachmentsMail(String to, String subject, String content, String filePath) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+
+            FileSystemResource file = new FileSystemResource(new File(filePath));
+            String fileName = filePath.substring(filePath.lastIndexOf(File.separator));
+            helper.addAttachment(fileName, file);
+            //helper.addAttachment("test"+fileName, file);
+
+            mailSender.send(message);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 发送正文中有静态资源（图片）的邮件
+     * @param to
+     * @param subject
+     * @param content
+     * @param rscPath
+     * @param rscId
+     */
+    @Override
+    public void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+
+            FileSystemResource res = new FileSystemResource(new File(rscPath));
+            helper.addInline(rscId, res);
+
+            mailSender.send(message);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
